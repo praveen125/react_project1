540c1a8ce0899933eb00883d7ceab17ba1618127
praveen125 <73333580+praveen125@users.noreply.github.com>
2021-11-15 14:15:14 +0530

    firstCommit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div className="form">
+        <nav
+          className="navbar"
+          class="navbar navbar-expand-lg navbar-dark bg-dark"
+        >
+          <ul className="nav">
+            <li>
+              <a class="navbar-brand" href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="navbar-brand" href="/MyProjects/">
+                MyProjects
+              </a>
+            </li>
+            <li>
+              <a class="navbar-brand" href="/clients/">
+                Clients
+              </a>
+            </li>
+            <li>
+              <a class="navbar-brand" href="/contact/">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
